b2e31555df5ac36f89e309f27ebb6a9d6664af8a
prateiku <pratikchopane1021@gmail.com>
2021-11-24 01:36:15 +0530

    Deletion Functionality

--- QuizApp/urls.py
+++ QuizApp/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('create/', views.create, name='create'),
+    path('delete/', views.DeleteQues, name='DeleteQues'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -21,3 +21,21 @@ def create(request):
     else:
         form = ExamForm()
     return render(request, 'create.html', {'form': form})
+
+
+def DeleteQues(request):
+    numba = Exam.objects.all()
+    ids = numba.values_list('QuestionNo', flat=True)
+    if request.method == 'POST':
+        form = ExamForm(request.POST)
+        QuesNo = request.POST.get('QuestionNo')
+        #print(type(QuesNo))
+        if int(QuesNo) in list(ids):
+            deleteQuest = Exam.objects.get(QuestionNo = QuesNo)
+            deleteQuest.delete()
+            messages.success(request,"Question Deleted successfully")
+        else:
+            messages.warning(request,"Question not found!!!")
+    else:
+        form = ExamForm()
+    return render(request, 'delete.html', {'form': form})
